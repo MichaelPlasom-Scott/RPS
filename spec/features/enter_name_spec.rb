bb100f6916462231997fe50db201d1c219776832
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-08-06 10:55:35 +0100

    player can enter name

---
+++ spec/features/enter_name_spec.rb
@@ -0,0 +1,13 @@
+# As a marketeer
+# So that I can see my name in lights
+# I would like to register my name before playing an online game
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_name, with: 'Michael'
+    click_button 'Submit'
+    save_and_open_page
+    expect(page).to have_content 'Michael! Rock, Paper, or Scissors?'
+  end
+end
