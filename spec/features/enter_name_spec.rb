c32b25f05669760b2f169e4ff015235e786bea19
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-08-06 10:59:53 +0100

    refactoring

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -7,7 +7,7 @@ feature 'Enter names' do
     visit('/')
     fill_in :player_name, with: 'Michael'
     click_button 'Submit'
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content 'Michael! Rock, Paper, or Scissors?'
   end
 end
