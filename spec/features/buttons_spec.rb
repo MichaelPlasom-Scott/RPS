ff50344bd23f1be8ce7584a5aeb8c977ac9075b7
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-08-06 12:39:22 +0100

    buttons for each option added, rubocop pass

---
+++ spec/features/buttons_spec.rb
@@ -0,0 +1,16 @@
+feature 'Display buttons for game' do
+  scenario 'Rock button' do
+    enter_name_and_submit
+    find_button('Rock').click
+  end
+
+  scenario 'Paper button' do
+    enter_name_and_submit
+    find_button('Paper').click
+  end
+
+  scenario 'Scissors button' do
+    enter_name_and_submit
+    find_button('Scissors').click
+  end
+end
